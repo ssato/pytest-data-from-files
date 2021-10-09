8d47915807c9ec1711aab28fe509c4301f25b58d
Satoru SATOH <satoru.satoh@gmail.com>
2021-10-09 13:57:10 +0900

    fix: export some data types, functions and fixtures explicitely

--- src/data_from_files/__init__.py
+++ src/data_from_files/__init__.py
@@ -2,8 +2,17 @@
 # Copyright (C) 2021 Satoru SATOH <satoru.satoh@gmail.com>
 # SPDX-License-Identifier: MIT
 #
-"""Fixtures and hooks to load test data."""
-from .fixtures import (  # noqa: F401
+"""Fixtures and hooks to load test data automatically."""
+from .datatypes import (
+    ModuleInfo, DataInfo
+)
+from .fixtures import (
     module_info, test_data
 )
-from .hooks import pytest_addoption  # noqa: F401
+from .hooks import pytest_addoption
+
+__all__ = [
+    'ModuleInfo', 'DataInfo',
+    'module_info', 'test_data',
+    'pytest_addoption',
+]
