5516ced656d5eb30985db52bc3b9f32b37f9a6fe
Satoru SATOH <satoru.satoh@gmail.com>
2021-10-09 13:23:54 +0900

    change: refer plugin name from tests.constants

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,9 @@
 #
 # pylint: disable=missing-docstring
 """conftest.py to load data_from_files.*."""
-pytest_plugins = ['data_from_files']
+from . import constants
+
+
+pytest_plugins = [constants.PLUGIN_NAME]
 
 # vim:sw=4:ts=4:et:
