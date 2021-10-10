62e09c716cd2154dc17b640277de242e401e8041
Satoru SATOH <satoru.satoh@gmail.com>
2021-10-10 22:42:33 +0900

    change: add tests.utils.concat and its test cases

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,10 +4,27 @@
 #
 # pylint: disable=missing-docstring
 """Test cases for tests.utils."""
+import pytest
+
 from . import utils as TT
 
 
 def test_get_basename():
     assert TT.get_basename(__file__) == 'utils'
+
+
+@pytest.mark.parametrize(
+    ('xss', 'exp'),
+    (
+     ([[]], []),
+     ((()), []),
+     ([[1, 2, 3], [4, 5]], [1, 2, 3, 4, 5]),
+     ([[1, 2, 3], [4, 5, [6, 7]]], [1, 2, 3, 4, 5, [6, 7]]),
+     (((1, 2, 3), (4, 5, (6, 7))), [1, 2, 3, 4, 5, (6, 7)]),
+     (((i, i * 2) for i in range(3)), [0, 0, 1, 2, 2, 4]),
+     )
+)
+def test_concat(xss, exp):
+    assert list(TT.concat(xss)) == exp
 
 # vim:sw=4:ts=4:et:
--- tests/utils.py
+++ tests/utils.py
@@ -4,11 +4,19 @@
 #
 # pylint: disable=missing-docstring
 """Common constant variables for test cases."""
+import itertools
 import pathlib
+import typing
 
 
 def get_basename(path: str) -> str:
     """Extract basename from given path ``path``."""
     return pathlib.Path(path).name.replace('.py', '').replace('test_', '')
+
+
+def concat(xss: typing.Iterable[typing.Iterable[typing.Any]]
+           ) -> typing.Iterator[typing.Any]:
+    """Concat an iterable of iterables."""
+    return itertools.chain.from_iterable(xs for xs in xss)
 
 # vim:sw=4:ts=4:et:
