deca8e3ae50351299f90bfe1561e5a32474812fa
Satoru SATOH <satoru.satoh@gmail.com>
2021-10-14 00:15:07 +0900

    fix: bump up the version to upload; 0.2.0 rc1

--- src/data_from_files/__init__.py
+++ src/data_from_files/__init__.py
@@ -12,7 +12,7 @@ from .fixtures import (
 from .hooks import pytest_addoption
 
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __all__ = [
     'ModuleInfo', 'DataInfo',
     'test_data_info', 'test_data',
